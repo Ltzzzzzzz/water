c255d94230f3471fde132708775bbc3f65a270be
刘添璋 <495236549@qq.com>
2020-09-28 10:14:52 +0800

    b

--- src/components/Navbar/src/index.vue
+++ src/components/Navbar/src/index.vue
@@ -7,12 +7,12 @@
 			<div class="goHome" v-if="$route.name !== 'Home'">
 				<router-link to="/">
 					<div class="goHomeWrapper">
-						<van-icon name="wap-home-o" size="20" color="#ffffff"></van-icon>
+						<van-icon name="wap-home-o" size="30" color="#ffffff"></van-icon>
 					</div>
 				</router-link>
 			</div>
 		</div>
-
+		<h2 class="routeName">{{ $route.meta.title }}</h2>
 		<slot></slot>
 	</div>
 </template>
@@ -36,8 +36,20 @@ export default {
 	min-height: 40vw;
 	background-image: linear-gradient(to right, #4199b4, #1d7eb8);
 	.titleWrapper {
+		margin-bottom: 10px;
 		display: flex;
 		justify-content: space-between;
+		.goHome {
+			padding: 5px;
+			background-color: #1974ab;
+			border-radius: 50%;
+		}
+	}
+	.routeName {
+		margin-bottom: 10px;
+		font-weight: normal;
+		font-size: 16px;
+		color: #ffffff;
 	}
 }
 </style>
